01a67a0cb42c96482e81110235522afbd1355d06
Suresh Shinde <suresh.socialchamps@gmail.com>
2016-08-01 15:56:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function sendGenericMessage(sender) {
 		"attachment": {
 			"type": "template",
 			"payload": {
-				"template_type": "generic",
+				"template_type": "Pics",
 				"elements": [{
 					"title": "Club House",
 					"subtitle": "Aminities( next)",
