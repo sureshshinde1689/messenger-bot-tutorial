6d1afca5f490b5e8df4a6108e8124c65b00f4d41
Suresh Shinde <suresh.socialchamps@gmail.com>
2016-08-04 15:19:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -54,19 +54,19 @@ app.post('/webhook/', function (req, res) {
 			else if (text=== 'What all amenities do you have?' || text=== 'aminities?' || text=== 'aminities'){
 				sendTextMessage(sender, "Clubhouse, swimming pool, amphitheater, gymnasium, garden, power backups for elevators etc.", token)
 			}
-			else if (text=== 'What are the areas of 1BHK and 2BHK Flats?'){
+			else if (text=== 'What are the areas of 1BHK and 2BHK Flats?' || text=== 'areas?' || text=== 'areas' || text=== 'area'){
 				sendTextMessage(sender, "1 BHK is 560 sq ft and 2 BHK is 950 Sq Ft.", token)
 			}
-			else if (text=== 'What are the rates?'){
+			else if (text=== 'What are the rates?' || text=== 'rates?' || text=== 'rates'){
 				sendTextMessage(sender, "1 BHK will be 28 Lakh (all inclusive) and 2BHK 46 Lakh (all inclusive)", token)
 			}
-			else if (text=== 'When will we get the possession?'){
+			else if (text=== 'When will we get the possession?' || text=== 'possession?' || text=== 'possession'){
 				sendTextMessage(sender, "Flats are ready possession flats.", token)
 			}
 			else if (text=== 'When can I see the site?'){
 				sendTextMessage(sender, "Sir, please let us know your convenient timings and we will book your site-visit accordingly.", token)
 			}
-			else if (text=== 'Do you have bank finance facility available?'){
+			else if (text=== 'Do you have bank finance facility available?' || text=== 'finance'){
 				sendTextMessage(sender, "Yes, you can get the home-loan assistance from HDFC, AXIS and SBI.", token)
 			}
 			else if (text=== 'Will you provide the services to do the site visit?'){
